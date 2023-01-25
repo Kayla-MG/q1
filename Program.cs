8892fad5829558328b41346f5c6c609cc6529bc7
Kayla-MG <S00233714@atu.ie>
2023-01-25 10:32:15 +0000

    part (a) (b) (c)

--- Program.cs
+++ Program.cs
@@ -1,10 +1,62 @@
 ﻿namespace q1
-{
+{//WORKSHEET1 Q1
+ //name - kayla mcgowan
+ //student no. - s00233714
+ /*Question 1
+    Each of these methods will take no parameters and will return void. 
+ 
+    Call the method from your main method to test it.
+a)  Write a method called PrintIntegersFor which uses a for loop to print all the integers between 40 and 60 in ascending order to the console.
+
+b)	Write a second method called PrintIntegersWhile which does the same thing using a while loop.
+
+c)	Write a third method, PrintIntegersDo, which does the same thing using a do..while loop.
+    Explain to the person beside you how these constructs differ.
+
+d)  Write a method, PrintExcludingFor, which uses a  for loop to print all integers between 40 and 60 excluding 46 and 48.
+
+e)	Write a method, PrintDescendingFor, which uses a for loop to print all integers between 60 and 40 in descending order.
+ */
+
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            PrintIntegersFor();
+            Console.WriteLine("**********************************************");
+            PrintIntegersWhile();
+            Console.WriteLine("**********************************************");
+            PrintIntegersDo();
+        }
+        static private void PrintIntegersFor()
+        {
+           // a)	Write a method called PrintIntegersFor which uses a for loop to print all the integers between 40 and 60 in ascending order to the console.
+            for (int i = 41; i < 60; i++)
+            {
+                Console.WriteLine($"{i}");
+            }
+        }
+        
+        static private void PrintIntegersWhile ()
+            //b)	Write a second method called PrintIntegersWhile which does the same thing using a while loop.
+        {
+            int i = 41;
+            while (i < 60)
+            {
+                Console.WriteLine(i);
+                i++;
+            }
+        }
+        static private void PrintIntegersDo()
+        {
+            //c)    Write a third method, PrintIntegersDo, which does the same thing using a do..while loop.
+            int i = 41;
+            do
+            {
+                Console.WriteLine(i);
+                i++;
+            }
+            while (i < 60);
         }
     }
 }
